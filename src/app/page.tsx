13927d0039c5aa1d81d29cf9562ac00955c4376a
andy <andersgee@gmail.com>
2024-06-30 18:29:15 +0200

    some padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { WindowAiChat } from "./window-ai-chat";
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
+    <main className="flex justify-center p-2">
       <div className="container">
         <h1 className="text-4xl">Gemini nano in-browser chat app</h1>
         <WindowAiChat />
